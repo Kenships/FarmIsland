e6a5863ee645a6cfe181e564ba4afd1034230d55
Zhaoqi Xu <kenships0402@gmail.com>
2024-01-24 22:58:18 -0500

    more java docs

--- Cursor.java
+++ Cursor.java
@@ -10,27 +10,43 @@ public class Cursor
 {
     private static Item holding;
     private static Tool activeTool;
-    
+    /**
+     * gets the mouse info
+     * @return MouseInfo
+     */
     public static MouseInfo getMouseInfo(){
         return Greenfoot.getMouseInfo();
     }
-    
+    /**
+     * releases the held item
+     */
     public static void release(){
         holding = null;
     }
-    
+        /**
+         * equips the tool
+         * @param tool
+         */
     public static void setTool(Tool tool){
         activeTool = tool;
     }
-    
+    /**
+     * gets the tool held
+     * @return Tool
+     */
     public static Tool getTool(){
         return activeTool;
     }
-    
+    /**
+     * gets the item held
+     * @return Item
+     */
     public static Item getItem(){
         return holding;
     }
-    
+    /**
+     * gets the Button last pressed
+     */
     public static int getButton(){
         MouseInfo mouse = Greenfoot.getMouseInfo();
         if(mouse != null){
@@ -41,18 +57,33 @@ public class Cursor
         }
         
     }
+    /**
+     * checks if the last click was a left click
+     * @return boolean was it left clicked
+     */
     
     public static boolean leftClicked(){
         return getButton() == 1;
     }
+    /**
+     * checks if the last click was a right click
+     * @return boolean was it right clicked
+     */
     public static boolean rightClicked(){
         return getButton() == 3;
     }
+    /**
+     * gets the click count of the mouse
+     * @return int the click couint
+     */
     public static int getClickCount(){
         MouseInfo mouse = Greenfoot.getMouseInfo();
         return mouse.getClickCount();
     }
-    
+    /**
+     * gets the X position of the mouse
+     * @return int 
+     */
     public static int getX(){
         MouseInfo mouse = Greenfoot.getMouseInfo();
         if(mouse != null){
@@ -62,7 +93,10 @@ public class Cursor
             return 0;
         }
     }
-    
+    /**
+     * gets the Y osition of the mouse
+     * @return int
+     */
     public static int getY(){
         MouseInfo mouse = Greenfoot.getMouseInfo();
          if(mouse != null){
@@ -72,7 +106,10 @@ public class Cursor
             return 0;
         }
     }
-    
+    /**
+     * picks up an item
+     * @param item the item to pick up
+     */
     public static void pickUp(Item item){
         holding = item;
     }
